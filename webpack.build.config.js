d72ef23bfa901a070e0882fe0b61f8980865b303
Zhenya Trofimov <deadrime@mail.ru>
2017-10-19 19:47:14 +0300

    small fix

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -26,7 +26,7 @@ module.exports = {
                 }]
             },
             { // css, postCss => css
-                test: /\.styl|.css$/,
+                test: /\.pcss|.css$/,
                 use: ExtractTextPlugin.extract({
                     use: [
                         {
